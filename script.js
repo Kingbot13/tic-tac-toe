15661fd68159a0c6b136a4711f715076a37a4628
Dylan King <dwkblue51@yahoo.com>
2021-07-10 21:53:42 -0500

    create function to render gameboard contents

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 /* 
     add gameboard object
         add array to store x's and o's
+            store each "box" as an object in array
     add game object to control game flow
         IIFE
     add player object (use factory funtion)
 */
 
 const Gameboard = (() => {
-    let arr = []; // store contents
-    const _renderContents = () => {
+    // let _boxes = document.querySelectorAll(".box");
+
+    // const _boxObj = () => {
+
+    // }
+
+    let arr = ["x", "o", "o", "x", "x", "o", "x", "o", "o"]; // store contents
+
+    const renderContents = () => {
         for (let i = 0; i < arr.length; i++){
             // check for content and render in appropriate div
+            let box = document.querySelector(`[data-key="${i.toString()}"]`);
+            box.textContent = `${arr[i]}`;
         };
     };
-    return {arr};
+    return {arr, renderContents};
 })();
+
+Gameboard.renderContents();
 
 const Player = (name) => {
     return {name};
@@ -26,4 +38,4 @@ const player2 = Player("player2");
 const Game = (() => {
     // control turns between player
     // push player selection to array
-})
+})();
