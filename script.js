eb99058db7b18053273a2be9ad5c8002dfb169f6
Dylan King <dwkblue51@yahoo.com>
2021-07-12 15:41:37 -0500

    add function to check for win

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-/* 
-    add gameboard object
-        add array to store x's and o's
-            store each "box" as an object in array
-    add game object to control game flow
-        
-    add player object (use factory funtion)
-*/
 
 const log = console.log;
 
 const Gameboard = (() => {
 
     let arr = ["", "", "", "", "", "", "", "", ""]; // store contents
+
+    const display = document.querySelector(".display");
 
     const renderContents = () => {
         for (let i = 0; i < arr.length; i++){
@@ -41,9 +35,7 @@ const player1 = Player("Player1");
 const player2 = Player("Player2");
 
 const Game = (() => {
-    // control turns between player
-    // push player selection to array
-    // add counter to track turns... odd numbers means player1, even means player2
+    // add counter to track turns... even numbers means player1, odd means player2
     let counter = 0;
 
     const boxes = document.querySelectorAll(".box");
@@ -59,38 +51,79 @@ const Game = (() => {
         if arr[3], arr[5], arr[7]
     */
 
+    const winConditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    
+    
     const checkWin = () => {
-        let arr = Gameboard.arr;
+        let winner;
+        const winMsg = `${winner} won the game!`;
+        const tieMsg = `It's a tie!`;
+        let gameMsg = document.createElement("p");
+        let gameOver = false;
         for (let i = 0; i < 3; i++){
-            if (arr[i] === arr[i + 1]){
-                
-            }
-        }
-    }
+            let condition = winConditions[i];
+            let a = condition[0];
+            let b = condition[1];
+            let c = condition[2];
+            if (a === "" || b === "" || c === ""){
+                continue;
+            } else if (a === b && b === c){
+                if (a === "x"){
+                    winner = player1;
+                } else {
+                    winner = player2;
+                };
+                gameOver = true;
+                gameMsg.textContent = winMsg;
+                break;
+            } else if (!Gameboard.arr.includes("")){
+                gameOver = true
+                gameMsg.textContent = tieMsg;
+            };
+        };
+        if (gameOver){
+            Gameboard.display.appendChild(gameMsg);
+        };
+        return gameOver;
+    };
 
     const play = () => {
         boxes.forEach((box) => {
             box.addEventListener("click", (e) => {
                 if (box.textContent === ""){
-                    let mark;
-                    if (counter % 2 === 0){
-                        mark = "x";
-                    } else {
-                        mark = "o"
+                    log(checkWin());
+                    if (!checkWin()){
+                        let mark;
+                        if (counter % 2 === 0){
+                            mark = "x";
+                        } else {
+                            mark = "o"
+                        };
+        
+                        // log(e.target);
+                        let key = parseInt(e.target.dataset.key);
+                        // log(key);
+                        // log(typeof(key));
+                        // log(mark);
+                        log(Gameboard.arr);
+                        Gameboard.arr.splice(key, 1, mark);
+                        Gameboard.renderContents();
+                        counter++;
+                        log(document.querySelector(`[data-key="8"]`).textContent);
+                        // log(counter);
+                        // log(playerSelection);
+
                     };
-    
-                    // log(e.target);
-                    let key = parseInt(e.target.dataset.key);
-                    // log(key);
-                    // log(typeof(key));
-                    // log(mark);
-                    log(Gameboard.arr);
-                    Gameboard.arr.splice(key, 1, mark);
-                    Gameboard.renderContents();
-                    counter++;
-                    log(document.querySelector(`[data-key="8"]`).textContent);
-                    // log(counter);
-                    // log(playerSelection);
 
                 };
             });  
